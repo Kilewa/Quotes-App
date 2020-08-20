2c533d659004f7de085ee56568ec39d09c88285d
kilewa <kilewageorge230@gmail.com>
2020-08-20 14:52:43 +0300

    added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QouteComponent } from './qoute/qoute.component';
+import { QuoteComponent } from './quote/quote.component';
 import { DetailComponent } from './detail/detail.component';
 import { FormComponent } from './form/form.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QouteComponent,
+    QuoteComponent,
     DetailComponent,
-    FormComponent
+    FormComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -2,6 +2,6 @@ import { Quote } from './quote';
 
 describe('Quote', () => {
   it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    expect(new Quote(this.quotePublisher, this.quoteAuthor, this.quoteStr)).toBeTruthy();
   });
 });
