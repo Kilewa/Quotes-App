9f850389524dcd243607c9f44dc15da674f964b5
kilewa <kilewageorge230@gmail.com>
2020-08-21 15:56:49 +0300

    added Hightlight directive logic

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(elem:ElementRef) {
+    elem.nativeElement.style.backgroundColor="#457052"
+    elem.nativeElement.style.padding="10px"
+    elem.nativeElement.style.borderRadius="5px"
+  }
 
 }
