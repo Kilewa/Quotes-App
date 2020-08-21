b66f84fd920554fe389a011716bc1916314d773d
kilewa <kilewageorge230@gmail.com>
2020-08-21 18:31:19 +0300

    highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -9,6 +9,7 @@ export class HighlightDirective {
     elem.nativeElement.style.backgroundColor="#457052"
     elem.nativeElement.style.padding="10px"
     elem.nativeElement.style.borderRadius="5px"
+    elem.nativeElement.style.marginTop="50px"
   }
 
 }
